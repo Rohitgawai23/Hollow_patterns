bcfa7c64a41c5af1efc4a54e5cb7b5f194b16a8d
Rohit Gawai <106953373+Rohitgawai23@users.noreply.github.com>
2022-07-09 12:08:36 +0530

    Add files via upload

---
+++ hollow_tringle.java
@@ -0,0 +1,28 @@
+/*
+
+*
+* *
+*   *
+*     *
+*       *
+*         *
+* * * * * * *
+*/
+class hollow_tringle
+{
+	public static void main(String [] args)
+	{
+		for(int i=1; i<=7; i++)	
+		{
+			for(int j=1; j<=i; j++)
+			{
+			
+				if(i==1 || j==1 || j==7 || i==1)
+					System.out.print("* ");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
